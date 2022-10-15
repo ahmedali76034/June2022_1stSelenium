66d11281c22e27181312e6a982d85d3fe4ef84d2
Ahmed Ali <ahmedali76034@gmail.com>
2022-10-15 16:55:18 -0500

    GitTest2

---
+++ src/main/java/page/GitTest2.java
@@ -0,0 +1,23 @@
+package page;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		printMe1();
+		printMe2();
+		printMe3();
+		
+	}
+
+	public static void printMe1() {
+		System.out.println("PrintMe");
+	}
+	
+	public static void printMe2() {
+		System.out.println("PrintMe2");
+	}
+	
+	public static void printMe3() {
+		System.out.println("PrintMe3");
+	}
+}
